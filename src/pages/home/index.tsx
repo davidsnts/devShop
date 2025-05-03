2f018a4cf0023809b61f59eefbe963576a2e3660
davidsnts <david.santos@delage.com.br>
2025-05-03 17:41:10 -0300

    Consumindo API

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,21 +1,50 @@
+import { useEffect, useState } from "react";
 import { BsCartPlus } from "react-icons/bs";
+import { api } from "../../services/api";
+
+interface ProductProps {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  cover: string;
+}
 
 export function Home() {
+  const [products, setProducts] = useState<ProductProps[]>([]);
+  useEffect(() => {
+    async function getProducts() {
+      const response = await api.get("/products");
+      setProducts(response.data);
+    }
+    getProducts();
+  }, []);
   return (
     <div>
       <main className="w-full max-w-7xl px-4 mx-auto">
-        <h1 className="font-bold text-2xl mb-4 mt-10 text-center">Produtos em alta</h1>
+        <h1 className="font-bold text-2xl mb-4 mt-10 text-center">
+          Produtos em alta
+        </h1>
 
         <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-5">
-          <section className="w-full">
-            <img className="w-full rounded-lg max-h-70 mb-2 " src="https://a-static.mlcdn.com.br/1500x1500/airpods-apple-com-estojo-de-recarga/magazineluiza/223026300/c07710dceefc3ded640be6a20db6a167.jpg" alt="Logo do Produto" />
+          {products &&
+            products.map((item) => (
+              <section key={item.id} className="w-full">
+                <img
+                  className="w-full rounded-lg max-h-70 mb-2 "
+                  src={item.cover}
+                  alt="Logo do Produto"
+                />
 
-            <p className="font-medium mt-1 mb-2">Airpods Pro</p>
-            <div className="flex gap-3 items-center">
-              <strong className="text-zinc-700/90">R$ 1000</strong>
-              <button className="bg-zinc-900 p-1 rounded"><BsCartPlus size={20} color="#FFF" /> </button>
-            </div>
-          </section>
+                <p className="font-medium mt-1 mb-2">{item.title}</p>
+                <div className="flex gap-3 items-center">
+                  <strong className="text-zinc-700/90">{item.price.toLocaleString("pt-BR", {style:"currency",currency: "brl"})}</strong>
+                  <button className="bg-zinc-900 p-1 rounded">
+                    <BsCartPlus size={20} color="#FFF" />{" "}
+                  </button>
+                </div>
+              </section>
+            ))}
         </div>
       </main>
     </div>
